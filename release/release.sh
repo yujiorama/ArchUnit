a25823f68113d6b7e64b00bac33895e450722c13
Peter Gafert <peter.gafert@tngtech.com>
2021-08-23 00:04:56 +0700

    add info statements to Gradle commands
    Signed-off-by: Peter Gafert <peter.gafert@tngtech.com>

--- release/release.sh
+++ release/release.sh
@@ -3,34 +3,34 @@
 set -e
 
 echo "Beginning"
-while [[ -n "${1-}" ]] ; do
+while [[ -n "${1-}" ]]; do
   case "${1}" in
-    --repository-username*)
-      SONATYPE_USERNAME=${1#*=}
-      ;;
-    --repository-password*)
-      SONATYPE_PASSWORD=${1#*=}
-      ;;
-    --signing-key*)
-      GPG_SIGNING_KEY=${1#*=}
-      ;;
-    --signing-password*)
-      GPG_SIGNING_PASSWORD=${1#*=}
-      ;;
-    *)
-      echo "Unknown option '${1}'"
-      exit 1
-      ;;
+  --repository-username*)
+    SONATYPE_USERNAME=${1#*=}
+    ;;
+  --repository-password*)
+    SONATYPE_PASSWORD=${1#*=}
+    ;;
+  --signing-key*)
+    GPG_SIGNING_KEY=${1#*=}
+    ;;
+  --signing-password*)
+    GPG_SIGNING_PASSWORD=${1#*=}
+    ;;
+  *)
+    echo "Unknown option '${1}'"
+    exit 1
+    ;;
   esac
   shift
 done
 
 if [ -n "$(git status --porcelain)" ]; then
-    echo "There are local, uncommitted changes, aborting..."
-    exit 1
+  echo "There are local, uncommitted changes, aborting..."
+  exit 1
 fi
 
-./gradlew prepareRelease
+./gradlew --info prepareRelease
 VERSION="$(grep -o -P '(?<=archunit\.version=).*' gradle.properties)"
 
 echo Committing prepared release
@@ -42,14 +42,14 @@ git tag "v$VERSION"
 git push origin "v$VERSION"
 
 echo Publishing ArchUnit...
-./gradlew clean publishArchUnit --no-parallel -PsonatypeUsername="$SONATYPE_USERNAME" -PsonatypePassword="$SONATYPE_PASSWORD" -PsigningKey="$GPG_SIGNING_KEY" -PsigningPassword="$GPG_SIGNING_PASSWORD"
+./gradlew --info clean publishArchUnit --no-parallel -PsonatypeUsername="$SONATYPE_USERNAME" -PsonatypePassword="$SONATYPE_PASSWORD" -PsigningKey="$GPG_SIGNING_KEY" -PsigningPassword="$GPG_SIGNING_PASSWORD"
 
 echo Publishing website and examples...
-./gradlew publishDocs
+./gradlew --info publishDocs
 (cd build/docs-update && git add -A && git commit -s -m "release ArchUnit $VERSION" && git push)
 (cd build/example-update && git add -A && git commit -s -m "release ArchUnit $VERSION" && git push)
 
-./gradlew setNextSnapshotVersion
+./gradlew --info setNextSnapshotVersion
 
 echo Committing new SNAPSHOT version
 git add -A
